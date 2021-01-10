64052a4ebb7a9cb1a583bd31bdb41d32112b1ec0
SaigyoujiYukon <40208202+SaigyoujiYukon@users.noreply.github.com>
2021-01-10 22:59:25 +0800

    Update wjx_auto_sub.user.js

--- wjx_auto_sub.user.js
+++ wjx_auto_sub.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Fuck问卷星
-// @version      0.11
+// @version      0.2
 // @description  问卷星链接自己填
 // @author       SaigyoujiYukon
-// @match        https://www.wjx.cn/jq/54616692.aspx
+// @match        https://www.wjx.cn/jq/85936460.aspx
+// @match        https://www.wjx.cn/m/85936460.aspx
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
@@ -30,16 +31,17 @@
 };
     var Shittime = new Date().Format("yyyy-MM-dd");
     const info=[
-        ["114514",/(您的姓名：)/],
+        ["阿巴阿巴",/(您的姓名：)/],
         [Shittime,/(请选择日期：)/],
-        ["确诊病例",/(确诊病例)/,/(无)/],
-        ["疑似病例",/(疑似病例)/,/(无)/],
+//      ["确诊病例",/(确诊病例)/,/(无)/],
+//      ["疑似病例",/(疑似病例)/,/(无)/],
         ["3",/(其他症..报，外伤\S在上报范围)/,/(无)/],
-        ["4",/(是否有抵穗人员报告\D\D祥看3号文\D\D含来源地点)/,/(否)/],
-        ["hh",/(本人居住单元有确诊、疑似和居家隔离病例情况含地点（是.否）)/,/(否)/],
-        ["[(√2-1)^0+(sin²1+cos²1+sin1cos1+1)！+3]^3 * (1+|∫<0, 2π>|cosx|dx)",/(学习强国分数，当前总得分)/],
+//      ["4",/(是否有抵穗人员报告\D\D祥看3号文\D\D含来源地点)/,/(否)/],
+//      ["hh",/(本人居住单元有确诊、疑似和居家隔离病例情况含地点（是.否）)/,/(否)/],
+//      ["[(√2-1)^0+(sin²1+cos²1+sin1cos1+1)！+3]^3 * (1+|∫<0, 2π>|cosx|dx)",/(学习强国分数，当前总得分)/],
+        ["8300",/(学习强国分数，当前总得分)/],
         ["¿",/(目前在哪里)/,/(在广州市)/],
-        ["hhh",/(2月1号之后是否在温州居住、停留或者途径温州)/,/(否)/]
+//      ["hhh",/(2月1号之后是否在温州居住、停留或者途径温州)/,/(否)/]
     ];
     const ini={
         module:".div_question",//模块
@@ -64,10 +66,10 @@
                        if(dom.length>0){
                            switch(tp){
                                case "input_text":
-                                   $("#q"+itemNum)[0].value = info[i][0]; 
+                                   $("#q"+itemNum)[0].value = info[i][0];
                                    break;
                                case "underline":
-                                   $("#q"+itemNum)[0].value = info[i][0]; 
+                                   $("#q"+itemNum)[0].value = info[i][0];
                                    break;
                                case "radio":
                                case "checkbox":
@@ -88,7 +90,16 @@
         });
     });
     setTimeout(function () {
+        window.scrollTo(0, document.documentElement.scrollHeight);
     	$('.submitbutton').click();//自动提交
+        $('.mainBgColor').click();//确认
         $('.rect-bottom').click();//自动验证
-   }, 1000);//这个延迟1秒比啥自动验证都好用
+        $('.sm-pop-close').click();//确认
+        setTimeout(function () {
+            $('.sm-pop-close').click();//确认
+            setTimeout(function () {
+                $('.rect-bottom').click();//自动验证
+            }, 3000);//延迟
+        }, 3000);//延迟
+   }, 5000);//延迟
 })();
